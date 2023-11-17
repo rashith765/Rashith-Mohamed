52790580a1a75ef85503dfc54409c42b08c9f12a
rashith765 / Rashith-Mohamed <rashithmohamed7@gmail.com>
2023-11-17 18:02:32 +0530

    day

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React from "react";
-import Header from "./component/header";
-import Main from "./component/main";
-import Footer from "./component/footer";
 
-import "./App.css"; 
+import Goku from "./day3/Goku";
+
+
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Main />
-      <Footer />
+    <div >
+     <Goku></Goku>
     </div>
   );
 }
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
   return (
     <div className="header">
-      <h1 style={headerStyles}>FORM</h1>
+      <h1 style={headerStyles}>Rashith</h1>
     </div>
   );
 }
